1501ff0615a174377821ec35c6d7a93819e837d1
sakshikasat <kasat.sakshi94@gmail.com>
2022-02-27 23:49:01 -0800

    fixing server startup issue

--- etsyback/server.js
+++ etsyback/server.js
@@ -8,8 +8,8 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const db = require('./app/models');
-db.sequilize.sync();
+const db = require('./models');
+db.sequelize.sync();
 
 app.get('/', (req, res) => {
     res.json({ message: 'This worked' });
