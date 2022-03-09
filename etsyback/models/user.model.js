355fb5e580cf877f55c36f8878c5b6b8e8b73549
Sakshi Kasat <kasat.sakshi94@gmail.com>
2022-03-09 09:57:34 -0800

    Fixing server startup issue

--- etsyback/models/user.model.js
+++ etsyback/models/user.model.js
@@ -1,16 +1,16 @@
-const { sequelize } = require('.');
+// const { sequelize } = require('.');
 
-module.exports = (seq) => {
-  const User = sequelize.define('users', {
-    name: {
-      type: seq.STRING,
-    },
-    email: {
-      type: seq.STRING,
-    },
-    gender: {
-      type: seq.STRING,
-    },
-  });
-  return User;
-};
+// module.exports = (seq) => {
+//   const User = sequelize.define('users', {
+//     name: {
+//       type: seq.STRING,
+//     },
+//     email: {
+//       type: seq.STRING,
+//     },
+//     gender: {
+//       type: seq.STRING,
+//     },
+//   });
+//   return User;
+// };
--- etsyback/server.js
+++ etsyback/server.js
@@ -9,12 +9,12 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const db = require('./models');
+// const db = require('./models');
 
-db.sequelize.sync();
+// db.sequelize.sync();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'This worked' });
+  res.json({ message: 'Etsy backend server is running' });
 });
 
 const PORT = process.env.PORT || 8080;
