dd91547c6171e314d41ad093c7f6807b51cf4114
Sakshi Kasat <kasat.sakshi94@gmail.com>
2022-03-10 01:16:24 -0800

    adding initial migration to create entire database schema and their tables

--- etsyback/migrations/20220308075714_init-database.js
+++ etsyback/migrations/20220308075714_init-database.js
@@ -2,17 +2,128 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
-  return Promise.all([
-    knex.schema.createTableIfNotExists('payment_paypal_status', (table) => {
-      table.increments(); // integer id
-
-      // name
-      table.string('name');
-
-      // description
-      table.string('description');
+exports.up = async function (knex) {
+   await Promise.all([
+     knex.schema.hasTable('address').then(function(exists) {
+      if (!exists) {
+        return knex.schema.createTable('address', (t) => {
+          t.increments().primary; // integer id
+          t.string('address1').notNullable();
+          t.string('address2');
+          t.string('city').notNullable();
+          t.string('state').notNullable();
+          t.string('country').notNullable();
+          t.string('zipcode').notNullable();
+          t.timestamp('createdAt').defaultTo(knex.fn.now());
+          t.timestamp('updatedAt').defaultTo(knex.fn.now());
+        })
+      }
     }),
+
+    knex.schema.hasTable('user').then(function(exists) {
+      if (!exists) {
+        return knex.schema.createTable('user', (t) => {
+          t.increments().primary; // integer id
+          t.string('name');
+          t.string('email').unique();
+          t.string('password').notNullable();
+          t.string('gender');
+          t.string('phone').checkRegex('[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]');
+          t.string('avatarUrl');
+          t.string('birthday');
+          t.string('bio');
+          t.string('currency').notNullable().defaultTo('USD');
+          t.boolean('userLevel').checkIn([0, 1, 2]); // 0-buyer, 1-seller, 2-admin
+          t.integer('addressId').unsigned();
+          t.foreign('addressId').references('id').inTable('address');
+          t.timestamp('createdAt').defaultTo(knex.fn.now());
+          t.timestamp('updatedAt').defaultTo(knex.fn.now());
+        })
+      }
+    }),
+
+    knex.schema.hasTable('shop').then(function(exists) {
+      if (!exists) {
+        return knex.schema.createTable('shop', (t) => {
+          t.increments().primary; // integer id
+          t.string('name').notNullable();
+          t.string('description');
+          t.string('avatarUrl');
+          t.integer('userId').unsigned();
+          t.integer('addressId').unsigned();
+          t.foreign('userId').references('id').inTable('user');
+          t.foreign('addressId').references('id').inTable('address');
+          t.timestamp('createdAt').defaultTo(knex.fn.now());
+          t.timestamp('updatedAt').defaultTo(knex.fn.now());
+        })
+      }
+    }),
+
+    knex.schema.hasTable('inventory').then(function(exists) {
+      if (!exists) {
+        return knex.schema.createTable('inventory', (t) => {
+          t.increments().primary; // integer id
+          t.string('name').notNullable();
+          t.string('description');
+          t.string('pictureUrl');
+          t.string('category').notNullable();
+          t.decimal('price').notNullable();
+          t.integer('quantity').notNullable();
+          t.integer('shopId').unsigned();
+          t.foreign('shopId').references('id').inTable('shop');
+          t.timestamp('createdAt').defaultTo(knex.fn.now());
+          t.timestamp('updatedAt').defaultTo(knex.fn.now());
+          t.timestamp('deletedAt');
+        })
+      }
+    }),
+
+    knex.schema.hasTable('userFavorites').then(function(exists) {
+      if (!exists) {
+        return knex.schema.createTable('userFavorites', (t) => {
+          t.increments().primary; // integer id
+          t.integer('userId').unsigned();
+          t.integer('inventoryId').unsigned();
+          t.foreign('userId').references('id').inTable('user');
+          t.foreign('inventoryId').references('id').inTable('inventory');
+          t.timestamp('createdAt').defaultTo(knex.fn.now());
+          t.timestamp('updatedAt').defaultTo(knex.fn.now());
+        })
+      }
+    }),
+
+    knex.schema.hasTable('order').then(function(exists) {
+      if (!exists) {
+        return knex.schema.createTable('order', (t) => {
+          t.increments().primary; // integer id
+          t.decimal('finalAmount').notNullable();
+          t.string('status').checkIn(['ordered', 'inTransit', 'delivered', 'cancelled']);
+          t.timestamp('orderedDate').notNullable();
+          t.integer('userId').unsigned();
+          t.integer('shopId').unsigned();
+          t.foreign('userId').references('id').inTable('user');
+          t.foreign('shopId').references('id').inTable('shop');
+          t.timestamp('createdAt').defaultTo(knex.fn.now());
+          t.timestamp('updatedAt').defaultTo(knex.fn.now());
+        })
+      }
+    }),
+
+    knex.schema.hasTable('orderDetails').then(function(exists) {
+      if (!exists) {
+        return knex.schema.createTable('orderDetails', (t) => {
+          t.increments().primary; // integer id
+          t.integer('orderQuantity').notNullable();
+          t.integer('orderId').unsigned();
+          t.integer('inventoryId').unsigned();
+          t.foreign('orderId').references('id').inTable('order');
+          t.foreign('inventoryId').references('id').inTable('inventory');
+          t.timestamp('createdAt').defaultTo(knex.fn.now());
+          t.timestamp('updatedAt').defaultTo(knex.fn.now());
+        })
+      }
+    }),
+
   ]);
 };
 
@@ -20,8 +131,14 @@ exports.up = function (knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {
-  return Promise.all([
-    knex.schema.dropTableIfExists('payment_paypal_status'),
+exports.down = async function (knex) {
+  await Promise.all([
+    await knex.schema.dropTableIfExists('orderDetails'),
+    await knex.schema.dropTableIfExists('order'),
+    await knex.schema.dropTableIfExists('userFavorites'),
+    await knex.schema.dropTableIfExists('inventory'),
+    await knex.schema.dropTableIfExists('shop'),
+    await knex.schema.dropTableIfExists('user'),
+    await knex.schema.dropTableIfExists('address'),
   ]);
 };
